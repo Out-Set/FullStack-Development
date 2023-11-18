3a25f3eb1717eef72a18ca71515b12ee1e69293f
swk802@gmail.com <swk802@gmail.com>
2023-11-18 23:57:54 +0530

    Started JSP

--- Front-End/6.Angular/ducatangular130/src/app/modules/services/myservice.service.ts
+++ Front-End/6.Angular/ducatangular130/src/app/modules/services/myservice.service.ts
@@ -6,24 +6,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MyserviceService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-path = "https://meanapps.onrender.com/getdata";
-path1 = "https://meanapps.onrender.com/view";
+  path = "https://meanapps.onrender.com/getdata";
+  path1 = "https://meanapps.onrender.com/view";
 
-myalldata()
-{
-  return this.http.get(this.path);
-}
+  myalldata() {
+    return this.http.get(this.path);
+  }
 
-singleuser(id:any)
-{
+  singleuser(id: any) {
 
-  return this.http.get(this.path1+"/"+id);
-}
-
-
-
-
+    return this.http.get(this.path1 + "/" + id);
+  }
 
 }
