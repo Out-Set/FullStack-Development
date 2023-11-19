85110ee8ba350bcf4d49d76c04cfb9331359f40b
swk802@gmail.com <swk802@gmail.com>
2023-11-20 00:55:49 +0530

    Modified result.js

--- Front-End/3.Java-Script/10.Call back function with objects/result.js
+++ Front-End/3.Java-Script/10.Call back function with objects/result.js
@@ -1,18 +1,18 @@
 var obj = [
-    { id: 1, Pname: "laptop", category: "laptop", subcategory: "hp", price: 30000, Pimage: "../images/l1.jpeg" },
-    { id: 2, Pname: "laptop", category: "laptop", subcategory: "hp", price: 32000, Pimage: "../images/l2.png" },
-    { id: 3, Pname: "laptop", category: "laptop", subcategory: "dell", price: 20000, Pimage: "../images/l3.jpeg" },
-    { id: 4, Pname: "laptop", category: "laptop", subcategory: "dell", price: 25000, Pimage: "../images/l4.jpeg" },
-    { id: 5, Pname: "laptop", category: "laptop", subcategory: "hp", price: 35000, Pimage: "../images/l5.jpeg" },
+    { id: 1, Pname: "laptop", category: "laptop", subcategory: "hp", price: 30000, Pimage: "product/l1.jpeg" },
+    { id: 2, Pname: "laptop", category: "laptop", subcategory: "hp", price: 32000, Pimage: "product/l2.png" },
+    { id: 3, Pname: "laptop", category: "laptop", subcategory: "dell", price: 20000, Pimage: "product/l3.jpeg" },
+    { id: 4, Pname: "laptop", category: "laptop", subcategory: "dell", price: 25000, Pimage: "product/l4.jpeg" },
+    { id: 5, Pname: "laptop", category: "laptop", subcategory: "hp", price: 35000, Pimage: "product/l5.jpeg" },
 
-    { id: 6, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 17000, Pimage: "../images/m1.jpeg" },
-    { id: 7, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 15000, Pimage: "../images/m2.jpeg" },
-    { id: 8, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 25000, Pimage: "../images/m3.jpeg" },
-    { id: 9, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 30000, Pimage: "../images/m4.jpeg" },
-    { id: 10, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 10000, Pimage: "../images/m5.jpeg" },
-    { id: 11, Pname: "watch", category: "watch", price: 2000, Pimage: "../images/w1.jpeg" },
+    { id: 6, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 17000, Pimage: "product/m1.jpeg" },
+    { id: 7, Pname: "mobile", category: "mobile", subcategory: "nokia", price: 15000, Pimage: "product/m2.jpeg" },
+    { id: 8, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 25000, Pimage: "product/m3.jpeg" },
+    { id: 9, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 30000, Pimage: "product/m4.jpeg" },
+    { id: 10, Pname: "mobile", category: "mobile", subcategory: "samsung", price: 10000, Pimage: "product/m5.jpeg" },
+    { id: 11, Pname: "watch", category: "watch", price: 2000, Pimage: "product/w1.jpeg" },
 
-    { id: 12, Pname: "watch", category: "watch", price: 3000, Pimage: "../images/w2.jpeg" }
+    { id: 12, Pname: "watch", category: "watch", price: 3000, Pimage: "product/w2.jpeg" }
 
 ]
 
