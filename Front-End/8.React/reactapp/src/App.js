25ee12c69a5898fcf937ba675d30d4322a116308
swk802@gmail.com <swk802@gmail.com>
2023-12-13 19:53:33 +0530

    Created function and class based component

--- Front-End/8.React/reactapp/src/App.js
+++ Front-End/8.React/reactapp/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Class based component(required render method)
+import React,{Component} from "react";
+
+class App extends Component{
+  render(){
+    return (
+      <h1>App Class Component</h1>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
+
+// Function based component(not required render method)
+/*
+function User(){
+  return(
+    <h2>User Component</h2>
+  )
+}
+
+export default User;
+*/
--- Front-End/8.React/reactapp/src/index.js
+++ Front-End/8.React/reactapp/src/index.js
@@ -1,3 +1,28 @@
+
+
+
+import App from "./App";
+import ReactDOM from 'react-dom';
+ReactDOM.render(<App/>, document.getElementById("root"))
+
+
+
+
+
+
+/*
+import User from'./App';
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<User/>, document.getElementById("root"));
+*/
+
+
+
+
+
+
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +40,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+
