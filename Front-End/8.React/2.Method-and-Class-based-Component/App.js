90da7c26648e037975c229d75dbe9cfd4f996d32
swk802@gmail.com <swk802@gmail.com>
2023-12-14 18:59:45 +0530

    Added 1-Lec

---
+++ Front-End/8.React/2.Method-and-Class-based-Component/App.js
@@ -0,0 +1,21 @@
+import React,{Component} from "react";
+
+class App extends Component{
+  render(){
+    return(
+      <h1>App class Component</h1>
+    )
+  }
+}
+export default App
+
+
+
+/*
+function User(){
+  return(
+    <h2>User component called</h2>
+  )
+}
+export default User
+*/ 
