67d2a8ad96f4f40e2f0a1ad591fd7e3df122bce5
swk802@gmail.com <swk802@gmail.com>
2024-01-24 23:34:50 +0530

    Completed Redux and end of React

---
+++ Front-End/8.React/react/myredux/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+import Data from './Data'
+function App() {
+  return (
+    <>
+      <h2>App Component</h2>
+      <hr />
+      <Counter />
+      <hr />
+      <Data />
+
+
+    </>
+  )
+}
+
+export default App
